7f3974c64fc787d6d604d718f431adcb3439285e
Dovid Edelkopf <dovidzedelkopf@gmail.com>
2023-01-10 13:02:15 -0500

    initialized react components to host the gameboard

---
+++ client/src/Gameboard.js
@@ -0,0 +1,13 @@
+
+
+function GameBoard() {
+
+    return (
+        <div className="gameBoard">
+            <div>I should probably put a navbar on here just for my peice of mind </div>
+            <div> game goes here </div>
+        </div>
+    )
+}
+
+export default GameBoard
