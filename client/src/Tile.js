a6bad78ba6463fe81955e4db565ff79429b9b1f0
Dovid Edelkopf <dovidzedelkopf@gmail.com>
2023-01-15 16:02:05 -0500

    refactored how users moves are tracked

--- client/src/Tile.js
+++ client/src/Tile.js
@@ -17,17 +17,32 @@ function Tile(
 
 
 
-    //chett said something about  having three possible states for a tile i.e. null, x, o 
+
 
 
 
 
 
     const compareArrays = () => {
-        for (let eachWinningBoard of boards) {
-            // eachWinningBoard.includes
-            console.log(eachWinningBoard.includes(usersMoves))
+
+        let index = 0
+        while (index < boards.length) {
+            console.log(boards[index])
+            usersMoves.includes(boards[index])
+            console.log(usersMoves.includes(boards[index]))
+            usersMoves.includes(boards[index])
+            index++
         }
+        // for (let eachWinningBoard of boards) {
+        //     // eachWinningBoard.includes
+
+        //     usersMoves.includes(eachWinningBoard) ? console.log("congrats! You won!") : console.log("you lost. better luck next time")
+        // console.log(usersMoves.includes(eachWinningBoard))
+
+
+        //if the return value from the above is true, console.log(congratz! You won!)
+        // else, end function
+        // }
     }
 
     //TODO: state that handles player making a move
@@ -39,21 +54,23 @@ function Tile(
         //when the player clicks on a tile, this is what happens 
 
         //the state for the turn is switched to computer turn. 
-        //this was turned off for testing purposes
+        //if this is commented out, it is for testing purposes
         // setPlayersTurn(false)
         // console.log(playersTurn)
 
 
         //the state for the tile clicked is switched from empty to full
+        //chett said something about  having three possible states for a tile i.e. null, x, o 
         setEmpty(false)
 
         //the id of the tile that was clicked is added to an state that contains an array of the user's moves
-        setUsersMoves([...usersMoves, `${tileid}`])
+        setUsersMoves([...usersMoves, tileid])
         // console.log(usersMoves)
 
         //the array of the user's moves is compared against a state that contains all the winning boards
         compareArrays()
         //if the user has a winning board, commence win/loss/tie state
+        // (this is handled in the compareArrays() function)
 
         //else, "COMPUTER is thinking about their next move"
         //computer selects a previously unselected tile
@@ -69,8 +86,6 @@ function Tile(
         //chett said something about  having three possible states for a tile i.e. null, x, o 
         console.log(`tile number ${tileid} was clicked`)
         console.log(tileid)
-
-
         console.log("is it my turn?", playersTurn)
     }
 
