c856698389840170087c73fbb4c7125627599938
Dovid Edelkopf <dovidzedelkopf@gmail.com>
2023-01-25 15:08:28 -0500

    presentation version

--- client/src/WinningScreen.js
+++ client/src/WinningScreen.js
@@ -8,14 +8,23 @@ function WinningScreen({playersTurn }) {
 
     const play = function() {
         new Audio("https://drive.google.com/uc?id=1GF_hpPMDb1cXwk3hqCeekIi76Ia8k53q&export=download").play()
+        setTimeout(() => {
+            console.log("delayed for 5 seconds")
+            new Audio("https://ia800200.us.archive.org/30/items/ShawtysLikeAMelody/shawtys_like_a_melody_in_my_head.mp3").play()
+        }, 3500 )
     }
 
+    // function shawtyLikeAMelody() {
+    //     new Audio("https://ia800200.us.archive.org/30/items/ShawtysLikeAMelody/shawtys_like_a_melody_in_my_head.mp3").play()
+    // }
 
     return (
-        <>
+        <div className='differentFlexDirection'>
         <div>{playersTurn ? "PLAYER 1 WON!" : "PLAYER 2 WON!"}</div>
-        <iframe src="https://giphy.com/embed/l1K9Dcy7ww0CW3JHq" width="480" height="406" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/reactionseditor-dancing-hot-dog-l1K9Dcy7ww0CW3JHq"></a></p>
-        </>
+        <iframe src="https://giphy.com/embed/l1K9Dcy7ww0CW3JHq" width="480" height="406" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+        <iframe src="https://giphy.com/embed/l1K9Dcy7ww0CW3JHq" width="480" height="406" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+        <iframe src="https://giphy.com/embed/l1K9Dcy7ww0CW3JHq" width="480" height="406" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+        </div>
     )
 }
 
